99ed0bf5bbd695ae0fcd0f100e76e4664cb694ae
Ivo Rocha <ivo.rocha@merkle.com>
2023-05-11 16:15:36 +0100

    fix: removed test requirements for now

--- test/commands/bcr/codeinsights/report.test.ts
+++ test/commands/bcr/codeinsights/report.test.ts
@@ -1,16 +1,3 @@
-import { expect, test } from '@oclif/test';
+// import { expect, test } from '@oclif/test';
 
-describe('bcr codeinsights report', () => {
-  // test
-  //   .stdout()
-  //   .command(['bcr codeinsights report'])
-  //   .it('runs hello', (ctx) => {
-  //     expect(ctx.stdout).to.contain('hello world');
-  //   });
-  // test
-  //   .stdout()
-  //   .command(['bcr codeinsights report', '--name', 'Astro'])
-  //   .it('runs hello --name Astro', (ctx) => {
-  //     expect(ctx.stdout).to.contain('hello Astro');
-  //   });
-});
+describe('bcr codeinsights report', () => {});
